5378560ff1634d31d89ce2094d62d5474f370332
Nischalj10 <nischal_j@ee.iitr.ac.in>
2022-03-29 15:24:57 +0530

    testing

--- client/src/pages/DocumentList.js
+++ client/src/pages/DocumentList.js
@@ -1,49 +1,61 @@
-import { useState } from 'react';
-import { Table, Button, ListGroup, Badge } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { decode } from 'jsonwebtoken';
-import NewDocModal from '../modals/NewDocModal';
-import NewCollaboratorModal from '../modals/NewCollaboratorModal';
-import EditCollaboratorModal from '../modals/EditCollaboratorModal';
-import { createDocument, setCollaborator, useCollaborators, useDocuments, useDocumentTitle, usePossibleCollaborators } from '../api/api';
+import { useState } from "react";
+import { Table, Button, ListGroup, Badge } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { decode } from "jsonwebtoken";
+import NewDocModal from "../modals/NewDocModal";
+import NewCollaboratorModal from "../modals/NewCollaboratorModal";
+import EditCollaboratorModal from "../modals/EditCollaboratorModal";
+import {
+  createDocument,
+  setCollaborator,
+  useCollaborators,
+  useDocuments,
+  useDocumentTitle,
+  usePossibleCollaborators,
+} from "../api/api";
 
 const ACCESS_TEXT = {
-  'manage': 'Manager',
-  'edit': 'Editor',
-  'view': 'Viewer'
+  manage: "Manager",
+  edit: "Editor",
+  view: "Viewer",
 };
 
 const ACCESS_VARIANT = {
-  'manage': 'primary',
-  'edit': 'info',
-  'view': 'secondary'
+  manage: "primary",
+  edit: "info",
+  view: "secondary",
 };
 
 const Collaborator = ({ username, access, onEdit }) => {
-  const editProps = (
-    typeof onEdit === 'function'
-      ? { action: true, onClick: onEdit }
-      : {}
-  );
+  const editProps =
+    typeof onEdit === "function" ? { action: true, onClick: onEdit } : {};
   return (
     <ListGroup.Item variant={ACCESS_VARIANT[access]} {...editProps}>
       {username}
-      <Badge variant={ACCESS_VARIANT[access]} className="ml-1">{ACCESS_TEXT[access]}</Badge>
+      <Badge variant={ACCESS_VARIANT[access]} className="ml-1">
+        {ACCESS_TEXT[access]}
+      </Badge>
     </ListGroup.Item>
   );
 };
 
 const Doc = ({ documentId, username }) => {
   const title = useDocumentTitle({ documentId });
-  const { collaborators, access, requestCollaborators } = useCollaborators({ documentId, username });
-  const isManager = access === 'manage';
+  const { collaborators, access, requestCollaborators } = useCollaborators({
+    documentId,
+    username,
+  });
+  const isManager = access === "manage";
   // separate out the other collaborators
   const others = collaborators.filter((c) => c.username !== username);
   // modal display flags
   const [addCollaborator, setAddCollaborator] = useState(false);
   const [editCollaborator, setEditCollaborator] = useState(undefined);
   // modal data
-  const users = usePossibleCollaborators({ collaborators, needed: addCollaborator });
+  const users = usePossibleCollaborators({
+    collaborators,
+    needed: addCollaborator,
+  });
   // update the collaborators
   const updateCollaborators = (username, access) => {
     setCollaborator({ documentId, username, access })
@@ -54,6 +66,7 @@ const Doc = ({ documentId, username }) => {
   return (
     <tr>
       <td>
+        {console.log()}
         <Link to={`/documents/${documentId}`}>{title}</Link>
       </td>
       <td>
@@ -61,14 +74,40 @@ const Doc = ({ documentId, username }) => {
       </td>
       <td>
         <ListGroup>
-          {others.map((c) => <Collaborator key={c.username} onEdit={isManager ? () => setEditCollaborator(c) : null} {...c} />)}
-          {isManager && <ListGroup.Item key="static" variant="light" action onClick={() => setAddCollaborator(true)}>Add Collaborator</ListGroup.Item>}
+          {others.map((c) => (
+            <Collaborator
+              key={c.username}
+              onEdit={isManager ? () => setEditCollaborator(c) : null}
+              {...c}
+            />
+          ))}
+          {isManager && (
+            <ListGroup.Item
+              key="static"
+              variant="light"
+              action
+              onClick={() => setAddCollaborator(true)}
+            >
+              Add Collaborator
+            </ListGroup.Item>
+          )}
         </ListGroup>
-        <NewCollaboratorModal show={addCollaborator} users={users} title={title} onUpdate={updateCollaborators} onHide={() => setAddCollaborator(false)} />
-        <EditCollaboratorModal collaborator={editCollaborator} title={title} onUpdate={updateCollaborators} onHide={() => setEditCollaborator(undefined)} />
+        <NewCollaboratorModal
+          show={addCollaborator}
+          users={users}
+          title={title}
+          onUpdate={updateCollaborators}
+          onHide={() => setAddCollaborator(false)}
+        />
+        <EditCollaboratorModal
+          collaborator={editCollaborator}
+          title={title}
+          onUpdate={updateCollaborators}
+          onHide={() => setEditCollaborator(undefined)}
+        />
       </td>
     </tr>
-  )
+  );
 };
 
 export default function DocumentList({ token }) {
@@ -95,13 +134,17 @@ export default function DocumentList({ token }) {
           </tr>
         </thead>
         <tbody>
-          {
-            documents.map((docUuid) => <Doc key={docUuid} documentId={docUuid} username={username} />)
-          }
+          {documents.map((docUuid) => (
+            <Doc key={docUuid} documentId={docUuid} username={username} />
+          ))}
         </tbody>
       </Table>
       <Button onClick={() => setAddDocument(true)}>Add New Document</Button>
-      <NewDocModal show={addDocument} addNewDocument={addNewDocument} cancelNewDocument={cancelNewDocument} />
+      <NewDocModal
+        show={addDocument}
+        addNewDocument={addNewDocument}
+        cancelNewDocument={cancelNewDocument}
+      />
     </>
   );
 }
